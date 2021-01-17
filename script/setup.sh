32644295042838104bc1ab3ca48bcc622563b2b3
sakiyamaK <sakiyama.k@gmail.com>
2021-01-17 13:04:30 +0900

    fix

--- script/setup.sh
+++ script/setup.sh
@@ -2,7 +2,7 @@
 mint bootstrap
 mint run xcodegen xcodegen generate
 bundle install
-bundle exec pod install
+env HOMEBREW_NO_AUTO_UPDATE=1 bundle exec pod install
 if [ "$?" -ne 0 ]; then
   echo "retry 'pod install' using '--repo-update'"
   bundle exec pod install --repo-update
